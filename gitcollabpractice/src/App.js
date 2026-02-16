b45ef836c8065d27be27cfad2473907922fb7eb2
2k22eee38-boop <2k22eee38@kiot.ac.in>
2026-02-16 20:17:17 +0530

    Added About Section

--- gitcollabpractice/src/App.js
+++ gitcollabpractice/src/App.js
@@ -1,13 +1,30 @@
+// import './App.css';
+// import NavBar from './components/NavBar';
+// import Home from './components/Home';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <NavBar />
+//       <Home />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <About />
     </div>
   );
 }
