b5fdbf6cf4d6efb6d33831ea22f58b2e700fb5ec
Dharshan20055 <dharshanmurali05@gmail.com>
2026-02-16 16:22:41 +0530

    contact added

--- gitcollabpractice/src/App.js
+++ gitcollabpractice/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import ContactPage from './components/ContactPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <ContactPage />
     </div>
   );
 }
